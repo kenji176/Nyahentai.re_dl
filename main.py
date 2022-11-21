303ccd3d8edc8ac791265dc4b377405c63a6c75a
sinc <73232388+kenji176@users.noreply.github.com>
2022-11-22 04:01:16 +0900

    Update main.py
    add Progressbar

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ from tqdm import tqdm
 from PIL import Image
 from bs4 import BeautifulSoup
 
-def callback(event):
-    th = threading.Thread(target=url_dl, args=(event,))
-    th.start()
+def Auto_Thread(func):
+    def wrapper(*args, **kwargs):
+        func_hl = Thread(target=func, args=args, kwargs=kwargs)
+        func_hl.start()
+        return func_hl
 
+    return wrapper
+
+@Auto_Thread
 def url_dl(url):
     if re.match("https://nyahentai.re/.*/.*/", url):
         if not (os.path.exists("./output")):
@@ -35,7 +40,10 @@ def url_dl(url):
         try:
             file_name = re.sub(r'[\\|/|:|?|.|"|<|>|\|]', '-', f'{Title}')
             with open(f'./output/{file_name}.pdf',"wb") as f:
-                for url in tqdm(img_url):
+                plabel['maximum'] = len(img_url)+1
+                plabel['value'] +=1
+                for i,url in  enumerate(img_url):
+                    plabel['value'] +=1
                     r = requests.get(url[0])
                     if r.status_code == 200:
                         bytes_output = io.BytesIO()
@@ -45,6 +53,7 @@ def url_dl(url):
                         bytes_images.append(bytes_output.getvalue())
                 f.write(img2pdf.convert(bytes_images))
                 messagebox.showinfo('成功', '保存が完了しました')
+                plabel.stop()
         except FileExistsError as e:
             print(f"同じ名前のフォルダが存在します,削除するか名前を変更してください\n[{Title}]")
     else:
@@ -60,13 +69,17 @@ txt.place(x=20, y=37)
 lbl = ttk.Label(text='Nyahentao_url')
 lbl.place(x=20, y=12)
 
-button = ttk.Button(root, text="DL",command=lambda : callback(txt.get()))
+button = ttk.Button(root, text="DL",command=lambda :url_dl(txt.get()))
 button.place(x=240, y=37)
 
-root.geometry('300x90')
+plabel = ttk.Progressbar(length=260,mode="determinate")
+plabel.pack(side="left")
+plabel.place(x=20, y=80)
+
+root.geometry('300x100')
 root.title('Nyahentai')
+
 sv_ttk.set_theme("dark")
 
 root.resizable(width=False, height=False)
 root.mainloop()
-
